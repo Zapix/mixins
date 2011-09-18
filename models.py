8d4ed289bccb745292084eba1795deb27a8da293
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2011-09-18 15:39:08 +0600

    Delete

--- models.py
+++
@@ -1,6 +0,0 @@
-form django.db import models
-from django.utils.translation import ugettext_lazy as _
-
-class CreatedUpdatedMixin(object):
-    created = models.DateTimeField(_('Created'), auto_now_add=True)
-    updated = models.DateTimeField(_('Updated'), auto_now=True)
