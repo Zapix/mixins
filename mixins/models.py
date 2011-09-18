ba57c7e391ca52ff5bd291f16576b12dad05682e
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2011-09-18 16:11:06 +0600

    Bug fix CreatedUpdatedMixin

--- mixins/models.py
+++ mixins/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-class CreatedUpdatedMixin(object):
+class CreatedUpdatedMixin(models.Model):
     created = models.DateTimeField(_('Created'), auto_now_add=True)
     updated = models.DateTimeField(_('Updated'), auto_now=True)
+    
+    class Meta:
+        abstract=True
