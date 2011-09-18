8363df1b60c60088ffbe08ebb7d9f2a98088bfe7
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2011-09-18 15:17:40 +0600

    Adding UpdatedCreatedMixin

---
+++ models.py
@@ -0,0 +1,6 @@
+form django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class CreatedUpdatedMixin(object):
+    created = models.DateTimeField(_('Created'), auto_now_add=True)
+    updated = models.DateTimeField(_('Updated'), auto_now=True)
